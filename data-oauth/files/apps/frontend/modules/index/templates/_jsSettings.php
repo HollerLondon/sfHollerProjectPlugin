b2cc63a7e76848e459996ed94c36983a68e7997e
Jo Carter <github@jocarter.co.uk>
2013-06-13 18:06:35 +0200

    Update _jsSettings.php
    Missing '

--- data-oauth/files/apps/frontend/modules/index/templates/_jsSettings.php
+++ data-oauth/files/apps/frontend/modules/index/templates/_jsSettings.php
@@ -3,6 +3,6 @@
     scope           : '<?php echo sfConfig::get('app_facebook_app_scope'); ?>',
     authUser        : '<?php echo url_for('@authUser'); ?>',
     homepage        : '<?php echo url_for('@homepage', true); ?>',
-    logout          : '<?php echo url_for('@sf_guard_signout'); ?>
+    logout          : '<?php echo url_for('@sf_guard_signout'); ?>'
   };
 </script>
